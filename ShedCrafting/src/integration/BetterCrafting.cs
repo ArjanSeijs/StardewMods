1fc3077b614bc111f437560de5dd3f895587d401
ArjanSeijs <arjan.seijs@gmail.com>
2026-05-31 15:56:09 +0200

    [ShedCrafting] Wrap OnMenuChanged in try catch

--- ShedCrafting/src/integration/BetterCrafting.cs
+++ ShedCrafting/src/integration/BetterCrafting.cs
@@ -13,38 +13,47 @@ public static class BetterCrafting
 
     public static void OnMenuChanged(MenuChangedEventArgs e)
     {
-        // We Just Tried to Replace it with our Page
-        if (_callFlag)
+        try
         {
-            _callFlag = false;
-            return;
+
+            // When we change the menu this function is called again.
+            // With this flag we avoid a infinite loop.
+            if (_callFlag)
+            {
+                _callFlag = false;
+                return;
+            }
+
+            // 
+            var api = ModEntry.Mod.BetterCraftingAPI;
+            var menu = api?.GetActiveMenu();
+            if (api is null || menu?.Position is null || menu.Location is null) return;
+
+            var chests = (from chest in ShedCrafting.GetChests(menu.Position.Value, menu.Location)
+                select new Tuple<object, GameLocation?>(chest, chest.Location)).ToList();
+            if (chests.Count == 0) return;
+
+            // Do The Replacing
+            _callFlag = true;
+            var success = api.OpenCraftingMenu(
+                menu.Cooking,
+                true,
+                menu.Location,
+                menu.Position,
+                menu.Area,
+                menu.DiscoverContainers,
+                chests,
+                menu.GetListedRecipes()?.ToList(),
+                menu.DiscoverBuildings
+            );
+            if (!success)
+            {
+                ModEntry.Mod.Monitor.Log("OnMenuChanged [BetterCrafting] Could not replace menu.", LogLevel.Debug);
+            }
         }
-
-        // 
-        var api = ModEntry.Mod.BetterCraftingAPI;
-        var menu = api?.GetActiveMenu();
-        if (api is null || menu?.Position is null || menu.Location is null) return;
-
-        var chests = (from chest in ShedCrafting.GetChests(menu.Position.Value, menu.Location)
-            select new Tuple<object, GameLocation?>(chest, chest.Location)).ToList();
-        if (chests.Count == 0) return;
-
-        // Do The Replacing
-        _callFlag = true;
-        var success = api.OpenCraftingMenu(
-            menu.Cooking,
-            true,
-            menu.Location,
-            menu.Position,
-            menu.Area,
-            menu.DiscoverContainers,
-            chests,
-            menu.GetListedRecipes()?.ToList(),
-            menu.DiscoverBuildings
-        );
-        if (!success)
+        catch (Exception ex)
         {
-            ModEntry.Mod.Monitor.Log("OnMenuChanged [BetterCrafting] Could not replace menu.", LogLevel.Debug);
+            ModEntry.Mod.Monitor.Log("OnMenuChanged Exception: " + ex, LogLevel.Error);
         }
     }
 }
